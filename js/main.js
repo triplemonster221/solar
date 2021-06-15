8eb02191234c2691a43b77a1ff40faeb30806cce
triplemonster221 <boorder1993@yandex.ru>
2021-06-15 14:31:54 +0300

    start footer

--- js/main.js
+++ js/main.js
@@ -18,6 +18,23 @@
          slidesToScroll: 1,
       });
 
+      $('.clients__slaider-sl').slick({
+         asNavFor:".clients__slaider-info",
+         dots: false,
+         prevArrow: '<button type="button" class="slick-prev arrows clients__arrows clients__prev"><img src="../images/arrow-left.png"></button>',
+         nextArrow: '<button type="button" class="slick-next arrows clients__arrows clients__next"><img src="../images/arrow-right.png"></button>',
+         arrows: true,
+         slidesToScroll: 1,
+      });
+
+      $('.clients__slaider-info').slick({
+         asNavFor:".clients__slaider-sl",
+         dots: false,  
+         arrows: false,       
+         fade: true,
+         slidesToScroll: 1,
+      });
+
 
       $('.rate__link').on('click', function(e){
          e.preventDefault();
